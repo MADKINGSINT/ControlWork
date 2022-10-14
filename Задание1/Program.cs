e3704b2974606b25c0cb0a6958518ae744624fd8
madkings <unconfigured@null.spigotmc.org>
2022-10-14 21:19:22 +0300

    started to write code

--- Задание1/Program.cs
+++ Задание1/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+
