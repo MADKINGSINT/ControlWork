6fe4cdad5078508a7c8bf3cf3962b9d131be22be
madkings <unconfigured@null.spigotmc.org>
2022-10-14 21:22:27 +0300

    part of code writed

--- Задание1/Program.cs
+++ Задание1/Program.cs
@@ -1,3 +1,12 @@
 ﻿Console.Write("Сколько элементов Вы хотите ввести? ");
 int size = Convert.ToInt32(Console.ReadLine());
-
+string[] array1 = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string ArrayPart = Convert.ToString(Console.ReadLine());
+    array1[i] = ArrayPart;
+}
+string[] array2 = new string[size];
+int num = 3;
+int count = 0;
